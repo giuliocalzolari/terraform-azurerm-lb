390f1e25cdd645cd45595a806f6b02803abc29c8
Max Guzman <maximiliano.guzman@walmart.com>
2018-12-05 14:41:59 -0300

    feat(terraform): first commit.

---
+++ main.tf
@@ -0,0 +1,69 @@
+data "azurerm_resource_group" "main" {
+  name = "${var.resource_group}"
+}
+
+resource "random_string" "id" {
+  length  = 6
+  lower   = true
+  upper   = false
+  number  = false
+  special = false
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                         = "${var.cluster_name}-${var.environment}-${random_string.id.result}-pip"
+  location                     = "${data.azurerm_resource_group.main.location}"
+  resource_group_name          = "${data.azurerm_resource_group.main.name}"
+  public_ip_address_allocation = "static"
+}
+
+resource "azurerm_lb" "load_balancer" {
+  name                = "${var.cluster_name}-${var.environment}-${random_string.id.result}-lb"
+  location            = "${data.azurerm_resource_group.main.location}"
+  resource_group_name = "${data.azurerm_resource_group.main.name}"
+
+  frontend_ip_configuration {
+    name                 = "${azurerm_public_ip.public_ip.name}"
+    public_ip_address_id = "${azurerm_public_ip.public_ip.id}"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "address_pool" {
+  name                = "${var.cluster_name}-${var.environment}-${random_string.id.result}-workers"
+  resource_group_name = "${data.azurerm_resource_group.main.name}"
+  loadbalancer_id     = "${azurerm_lb.load_balancer.id}"
+}
+
+resource "azurerm_lb_probe" "lb_probe" {
+  name                = "${var.cluster_name}-${var.environment}-${random_string.id.result}-lb-probe"
+  resource_group_name = "${data.azurerm_resource_group.main.name}"
+  loadbalancer_id     = "${azurerm_lb.load_balancer.id}"
+  protocol            = "http"
+  request_path        = "/"
+  port                = "${var.lb_probe_port}"
+  interval_in_seconds = 15
+}
+
+resource "azurerm_lb_rule" "lb_rule_http" {
+  name                           = "${var.cluster_name}-${var.environment}-${random_string.id.result}-lb-rule-http"
+  resource_group_name            = "${data.azurerm_resource_group.main.name}"
+  loadbalancer_id                = "${azurerm_lb.load_balancer.id}"
+  protocol                       = "tcp"
+  frontend_port                  = 80
+  backend_port                   = "${var.lb_rule_port_http}"
+  frontend_ip_configuration_name = "${azurerm_public_ip.public_ip.name}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.address_pool.id}"
+  probe_id                       = "${azurerm_lb_probe.lb_probe.id}"
+}
+
+resource "azurerm_lb_rule" "lb_rule_https" {
+  name                           = "${var.cluster_name}-${var.environment}-${random_string.id.result}-lb-rule-https"
+  resource_group_name            = "${data.azurerm_resource_group.main.name}"
+  loadbalancer_id                = "${azurerm_lb.load_balancer.id}"
+  protocol                       = "tcp"
+  frontend_port                  = 443
+  backend_port                   = "${var.lb_rule_port_https}"
+  frontend_ip_configuration_name = "${azurerm_public_ip.public_ip.name}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.address_pool.id}"
+  probe_id                       = "${azurerm_lb_probe.lb_probe.id}"
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "cluster_name" {
+  type = "string"
+}
+
+variable "environment" {
+  type = "string"
+}
+
+variable "resource_group" {
+  type = "string"
+}
+
+variable "lb_probe_port" {
+  type = "string"
+}
+
+variable "lb_rule_port_http" {
+  type = "string"
+}
+
+variable "lb_rule_port_https" {
+  type = "string"
+}
