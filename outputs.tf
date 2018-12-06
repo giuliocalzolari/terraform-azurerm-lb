858f3b26c51e778fbbed06c541946ba47b285474
Max Guzman <maximiliano.guzman@walmart.com>
2018-12-06 11:07:45 -0300

    feat(terraform): add lb address pool id output.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "load_balancer_ip" {
   value = "${azurerm_public_ip.public_ip.ip_address}"
 }
+
+output "lb_address_pool_id" {
+  value = "${azurerm_lb_backend_address_pool.address_pool.id}"
+}
