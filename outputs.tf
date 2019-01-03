bfe0667e0efde3f330a2c675c00fe69a4b939ddc
Max Guzman <maximiliano.guzman@walmart.com>
2019-01-03 08:16:26 -0300

    feat(terraform): add load balancer rules variables to module output.

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,15 @@ output "load_balancer_ip" {
 output "lb_address_pool_id" {
   value = "${azurerm_lb_backend_address_pool.address_pool.id}"
 }
+
+output "lb_id" {
+  value = "${azurerm_lb.load_balancer.id}"
+}
+
+output "lb_frontend_ip_configuration_name" {
+  value = "${azurerm_public_ip.public_ip.name}"
+}
+
+output "lb_probe_id" {
+  value = "${azurerm_lb_probe.lb_probe.id}"
+}
