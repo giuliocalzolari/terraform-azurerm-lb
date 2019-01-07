74a85e7ba413b4657792c559f020cdaaeb3d3aa0
Max Guzman <maximiliano.guzman@walmart.com>
2019-01-07 09:19:28 -0300

    fix(terraform): correct frontend ip configuration name reference.

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_lb_rule" "lb_rule" {
   protocol                       = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 1)}"
   frontend_port                  = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 0)}"
   backend_port                   = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 2)}"
-  frontend_ip_configuration_name = "${var.frontend_name}"
+  frontend_ip_configuration_name = "${azurerm_lb.load_balancer.frontend_ip_configuration.name}"
   enable_floating_ip             = false
   backend_address_pool_id        = "${azurerm_lb_backend_address_pool.address_pool.id}"
   idle_timeout_in_minutes        = 5
