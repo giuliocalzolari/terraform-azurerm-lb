0859d93db3afcf62d335e065e79b18d48bf6a5c0
Max Guzman <maximiliano.guzman@walmart.com>
2019-01-07 13:43:34 -0300

    fix(terraform): remove lb type from name interpolation.

--- main.tf
+++ main.tf
@@ -4,19 +4,19 @@ data "azurerm_resource_group" "main" {
 
 resource "azurerm_public_ip" "public_ip" {
   count                        = "${var.lb_type == "public" ? 1 : 0}"
-  name                         = "${var.cluster_name}-${var.environment}-${var.name_suffix}-${var.lb_type}-pip"
+  name                         = "${var.cluster_name}-${var.environment}-${var.name_suffix}-pip"
   location                     = "${data.azurerm_resource_group.main.location}"
   resource_group_name          = "${data.azurerm_resource_group.main.name}"
   public_ip_address_allocation = "static"
 }
 
 resource "azurerm_lb" "load_balancer" {
-  name                = "${var.cluster_name}-${var.environment}-${var.name_suffix}-${var.lb_type}-lb"
+  name                = "${var.cluster_name}-${var.environment}-${var.name_suffix}-lb"
   location            = "${data.azurerm_resource_group.main.location}"
   resource_group_name = "${data.azurerm_resource_group.main.name}"
 
   frontend_ip_configuration {
-    name                          = "${var.cluster_name}-${var.environment}-${var.name_suffix}-${var.lb_type}-frontend"
+    name                          = "${var.cluster_name}-${var.environment}-${var.name_suffix}-frontend"
     public_ip_address_id          = "${var.lb_type == "public" ? join("",azurerm_public_ip.public_ip.*.id) : ""}"
     subnet_id                     = "${var.frontend_subnet_id}"
     private_ip_address            = "${var.frontend_private_ip_address}"
@@ -49,7 +49,7 @@ resource "azurerm_lb_rule" "lb_rule" {
   protocol                       = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 1)}"
   frontend_port                  = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 0)}"
   backend_port                   = "${element(var.lb_port["${element(keys(var.lb_port), count.index)}"], 2)}"
-  frontend_ip_configuration_name = "${var.cluster_name}-${var.environment}-${var.name_suffix}-${var.lb_type}-frontend"
+  frontend_ip_configuration_name = "${var.cluster_name}-${var.environment}-${var.name_suffix}-frontend"
   enable_floating_ip             = false
   backend_address_pool_id        = "${azurerm_lb_backend_address_pool.address_pool.id}"
   idle_timeout_in_minutes        = 5
